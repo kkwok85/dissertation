335a7fd3eccf5e3a2412115dc26a7e14b089a5be
kkwok85 <kkwok85@gmail.com>
2016-01-08 20:10:18 -0500

    Jan 8 2016

--- script_ATUS/clean_data.R
+++ script_ATUS/clean_data.R
@@ -19,30 +19,46 @@ merged.data <- merge(atusact, atussum, by="tucaseid")
 #https://cran.rstudio.com/web/packages/dplyr/vignettes/introduction.html
 #http://stackoverflow.com/questions/1660124/how-to-sum-a-variable-by-group
 # http://www.bls.gov/tus/lexiconnoex0314.pdf
-050101 is main job
+# 050101 is main job
 # tewhere = 1 is home = 2 is workplace
 
 # check dplyr and time the merge time
 
 
 
+ID.list <- as.data.frame(atussum$tucaseid)
+colnames(ID.list) <- "ID"
+
 
 sum.main.job <- aggregate(atusact$tuactdur24, by=list( ID = atusact$tucaseid , code = atusact$trcodep ==50101  ), FUN=sum)
 
-sum.main.job$main <- 1440 - sum.main.job$x
+sum.main.job$main.job.total.time <- 1440 - sum.main.job$x
 
 
 
+sum.main.job.true.only <- subset(sum.main.job, sum.main.job$code == TRUE )
+sum.main.job.true.only <- sum.main.job.true.only[c("ID", "code", "x")] 
 
-sum.main.job2 <- subset(sum.main.job, sum.main.job$code == TRUE )
-  
-sum.main.job3 <- subset(sum.main.job, sum.main.job$code == FALSE )
+# both are ok
+sum.main.job.false.only <- subset(sum.main.job, sum.main.job$code == FALSE )
+
+sum.main.job.merge <- merge(ID.list, sum.main.job.true.only , by="ID", all = T)
+sum.main.job.merge[is.na(sum.main.job.merge)] <- 0
+
 
 # for checking only
-table(sum.main.job3$main-atussum$t050101)
+table(sum.main.job.false.only$main.job.total.time-atussum$t050101)
+table(sum.main.job.merge$x-atussum$t050101)
 
 
-sum.main.job <- aggregate(atusact$tuactdur24, by=list( ID = atusact$tucaseid , code = atusact$trcodep ==50101  ), FUN=sum)
+
+
+
+
+
+
+
+
 
 
 
@@ -53,6 +69,20 @@ sum.main.job <- aggregate(atusact$tuactdur24, by=list( ID = atusact$tucaseid , c
 
 sum.main.job.home <- aggregate(atusact$tuactdur24, by=list( ID = atusact$tucaseid , code = atusact$trcodep ==50101, location = atusact$tewhere == 1  ), FUN=sum)
 
-sum.main.job.office <- aggregate(atusact$tuactdur24, by=list( ID = atusact$tucaseid , code = atusact$trcodep ==50101, location = atusact$tewhere == 2  ), FUN=sum)
+sum.main.job.home.2 <- subset(sum.main.job.home, sum.main.job.home$code == TRUE & sum.main.job.home$location == TRUE)
+
+
+
+
+
+sum.main.job.home.3 <- merge(sum.main.job.merge, sum.main.job.home.2, by="ID", all = T)
+
+sum.main.job.home.3[is.na(sum.main.job.home.3 )] <- 0
+
+
+sum.main.job.home.3$percent.main.job.wfh <- sum.main.job.home.3$x.y/sum.main.job.home.3$x.x
+
+
+
 
 
