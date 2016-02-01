3b1a1c92f4f2b5d3a54cbd9f7ca15dc01a06b641
kkwok85 <kkwok85@gmail.com>
2016-02-01 00:37:56 -0500

    Fed 1 2016

--- script_ATUS/regression_script.R
+++ script_ATUS/regression_script.R
@@ -34,12 +34,43 @@ for (i in 1:length(yvariables) ) {
 
 
 
-stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]], result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
-          title = "OLS results for working from home", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]], robust.result[[8]],
-                                                                robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
-          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7], yvariables[8], yvariables[9], yvariables[10],
-                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
-          keep= c("wfh_mom" ), covariate.labels =c("Work from home(dummy)","Unemployed/Not in labor force(dummy)"),  type="latex", out = "D:/ATUS_result_R/result_wfh_mom.tex")
+#stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]], result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
+#          title = "OLS results for working from home", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]], robust.result[[8]],
+#                                                                robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
+#          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7], yvariables[8], yvariables[9], yvariables[10],
+#                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
+#          keep= c("wfh_mom" ), covariate.labels =c("Work from home(dummy version1)","Unemployed/Not in labor force(dummy)"),  type="latex", out = "D:/ATUS_result_R/result_wfh_mom.tex",
+#          notes = "Sampling weights are applied and robust standard errors are used",  notes.align = "l")
+
+
+
+
+
+
+
+
+stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]],
+          title = "OLS results for working from home (version 1)", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]]),
+          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7]), 
+          keep= c("wfh_mom" ), covariate.labels =c("Work from home(dummy version 1)","Unemployed/Not in labor force(dummy)"),  type="latex", out = "D:/ATUS_result_R/result_wfh_mom1.tex",
+          notes = "Sampling weights are applied and robust standard errors are used", notes.align = "l")
+
+
+stargazer(result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
+          title = "OLS results for working from home (version 1)", se = list( robust.result[[8]], robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
+          column.labels=c(yvariables[8], yvariables[9], yvariables[10], yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
+          keep= c("wfh_mom" ), covariate.labels =c("Work from home(dummy version 1)","Unemployed/Not in labor force(dummy)"),  type="latex", out = "D:/ATUS_result_R/result_wfh_mom2.tex",
+          notes = "Sampling weights are applied and robust standard errors are used", notes.align = "l")
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -62,13 +93,22 @@ for (i in 1:length(yvariables) ) {
 
 
 
-stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]], result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
-          title = "OLS results for working from home", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]], robust.result[[8]],
-                                           robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
-          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7], yvariables[8], yvariables[9], yvariables[10],
-                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
-          keep= c("wfh_mom.v2" ), covariate.labels =c("Work from home(dummy)","Unemployed/Not in labor force(dummy)") , type="latex", out = "D:/ATUS_result_R/result_wfh_mom.v2.tex")
+stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]],
+          title = "OLS results for working from home (version 2)", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]]),
+          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7]), 
+          keep= c("wfh_mom.v2" ), covariate.labels =c("Work from home(dummy version 2)","Unemployed/Not in labor force(dummy)") , type="latex", out = "D:/ATUS_result_R/result_wfh_mom.v2_1.tex",
+          notes = "Sampling weights are applied and robust standard errors are used" , notes.align = "l")
 
+
+
+
+
+stargazer(result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
+          title = "OLS results for working from home (version 2)", se = list(robust.result[[8]], robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
+          column.labels=c(yvariables[8], yvariables[9], yvariables[10],
+                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
+          keep= c("wfh_mom.v2" ), covariate.labels =c("Work from home(dummy version 2)","Unemployed/Not in labor force(dummy)") , type="latex", out = "D:/ATUS_result_R/result_wfh_mom.v2_2.tex",
+          notes = "Sampling weights are applied and robust standard errors are used", notes.align = "l")
 
 
 
@@ -107,7 +147,7 @@ stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result
 ############################
 # Regression for working status definition
 
-# controls slightly changed!working hours is dropped!
+# controls slightly changed!working hours is dropped! work from home is added!
 
 
 result <- list()
@@ -115,7 +155,7 @@ robust.result <- list()
 
 for (i in 1:length(yvariables) ) {
   dep.var <- yvariables[i]
-  full.model <- lm(paste0(yvariables[i]," ~ edited.employ.status", demographic.var, "+ edited.occupations + factor(edited.occupations.indicator)",family.var, time.var, location.var, spouse.var, family.inc.var,  childcare.service.var ), data = combine.data.regress, weights = tufnwgtp )
+  full.model <- lm(paste0(yvariables[i]," ~ edited.employ.status + wfh_mom.v2", demographic.var, "+ edited.occupations + factor(edited.occupations.indicator)",family.var, time.var, location.var, spouse.var, family.inc.var,  childcare.service.var ), data = combine.data.regress, weights = tufnwgtp )
   result[[i]] <- full.model
   robust.result[[i]] <- sqrt(diag(vcovHC(full.model, type = "HC1"))) 
 }
@@ -127,14 +167,33 @@ for (i in 1:length(yvariables) ) {
 
 
 
-stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]], result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
-          title = "OLS results for employed status", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]], robust.result[[8]],
-                                                                 robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
-          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7], yvariables[8], yvariables[9], yvariables[10],
-                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
-          keep= c("edited.employ.status" ), covariate.labels =c("Employed(dummy)") , type="latex", out = "D:/ATUS_result_R/result_edited.employ.status.tex")
+#stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]], result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
+#          title = "OLS results for employed status", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]], robust.result[[8]],
+#                                                                 robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
+#          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7], yvariables[8], yvariables[9], yvariables[10],
+#                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
+#          keep= c("edited.employ.status" ), covariate.labels =c("Employed(dummy)") , type="latex", out = "D:/ATUS_result_R/result_edited.employ.status.tex")
 
 
+
+
+
+
+
+stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]],
+          title = "OLS results for employed status", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]]),
+          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7]), 
+          keep= c("edited.employ.status" ), covariate.labels =c("Employed(dummy)") , type="latex", out = "D:/ATUS_result_R/result_edited.employ.status1.tex",
+          notes = "Sampling weights are applied and robust standard errors are used", notes.align = "l")
+
+
+
+
+stargazer(result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
+          title = "OLS results for employed status", se = list(robust.result[[8]], robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
+          column.labels=c(yvariables[8], yvariables[9], yvariables[10],yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
+          keep= c("edited.employ.status" ), covariate.labels =c("Employed(dummy)") , type="latex", out = "D:/ATUS_result_R/result_edited.employ.status2.tex",
+          notes = "Sampling weights are applied and robust standard errors are used", notes.align = "l")
 
 
 
@@ -264,12 +323,30 @@ for (i in 1:length(yvariables) ) {
 
 
 
-stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]], result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
-          title = "OLS results for interaction of working from home and spouse working hours", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]], robust.result[[8]],
-                                                                                                    robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
-          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7], yvariables[8], yvariables[9], yvariables[10],
-                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
-          keep= c("wfh_mom.v2", "edited.spouse.work.hours" ), covariate.labels =c("Work from home(dummy)","Unemployed/Not in labor force(dummy)", "Spouse work hours", "Indicator of spouse hours imputed","Work from home*Spouse work hours",  "Unemployed/Not in labor force*Spouse work hours") , type="latex", out = "D:/ATUS_result_R/result_interaction_wfh_spouse.wh.tex")
+#stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]], result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
+#          title = "OLS results for interaction of working from home and spouse working hours", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]], robust.result[[8]],
+#                                                                                                    robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
+#          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7], yvariables[8], yvariables[9], yvariables[10],
+#                          yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
+#          keep= c("wfh_mom.v2", "edited.spouse.work.hours" ), covariate.labels =c("Work from home(dummy)","Unemployed/Not in labor force(dummy)", "Spouse work hours", "Indicator of spouse hours imputed","Work from home*Spouse work hours",  "Unemployed/Not in labor force*Spouse work hours") , type="latex", out = "D:/ATUS_result_R/result_interaction_wfh_spouse.wh.tex")
+
+
+
+stargazer(result[[1]], result[[2]],result[[3]], result[[4]], result[[5]], result[[6]], result[[7]],
+          title = "OLS results for interaction of working from home and spouse working hours", se = list(robust.result[[1]], robust.result[[2]],robust.result[[3]], robust.result[[4]], robust.result[[5]], robust.result[[6]], robust.result[[7]]),
+          column.labels=c(yvariables[1],yvariables[2], yvariables[3], yvariables[4], yvariables[5], yvariables[6], yvariables[7]), 
+          keep= c("wfh_mom.v2", "edited.spouse.work.hours" ), covariate.labels =c("Work from home(dummy)","Unemployed/Not in labor force(dummy)", "Spouse work hours", "Indicator of spouse hours imputed","Work from home*Spouse work hours",  "Unemployed/Not in labor force*Spouse work hours") , type="latex", out = "D:/ATUS_result_R/result_interaction_wfh_spouse.wh1.tex",
+          notes = "Sampling weights are applied and robust standard errors are used", notes.align = "l")
+
+
+
+stargazer(result[[8]], result[[9]], result[[10]], result[[11]], result[[12]], result[[13]], result[[14]],
+          title = "OLS results for interaction of working from home and spouse working hours", se = list(robust.result[[8]],robust.result[[9]], robust.result[[10]], robust.result[[11]], robust.result[[12]], robust.result[[13]], robust.result[[14]]),
+          column.labels=c(yvariables[8], yvariables[9], yvariables[10],yvariables[11], yvariables[12], yvariables[13], yvariables[14]), 
+          keep= c("wfh_mom.v2", "edited.spouse.work.hours" ), covariate.labels =c("Work from home(dummy)","Unemployed/Not in labor force(dummy)", "Spouse work hours", "Indicator of spouse hours imputed","Work from home*Spouse work hours",  "Unemployed/Not in labor force*Spouse work hours") , type="latex", out = "D:/ATUS_result_R/result_interaction_wfh_spouse.wh2.tex",
+          notes = "Sampling weights are applied and robust standard errors are used", notes.align = "l")
+
+
 
 
 
